6ab66d996ba84d3260e8539581705ff59e92ed6a
ihasidul <ihasidul@gmail.com>
2025-06-08 02:17:10 +0100

    Add formatting and installed tanstack query

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import Pizza from "./Pizza";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
--- practice/src/Header.jsx
+++ practice/src/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
   const [cart] = useContext(CartContext);
   return (
     <nav>
-    <Link to="/">
-      <h1 className="logo">Padre Gino's Pizza</h1>
-    </Link>
+      <Link to="/">
+        <h1 className="logo">Padre Gino's Pizza</h1>
+      </Link>
       <div className="nav-cart">
         🛒<span className="nav-cart-number">{cart.length}</span>
       </div>
