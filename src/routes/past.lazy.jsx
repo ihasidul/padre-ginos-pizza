eec250fa54985be04a728719fdaf56fbf77fe0ed
ihasidul <ihasidul@gmail.com>
2025-06-09 07:58:43 +0100

    Add the view order option using modal

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { createLazyFileRoute } from '@tanstack/react-router'
 import getPastOrders from '../api/getPastOrders'
+import getPastOrder from '../api/getPastOrder'
+import { priceConverter } from '../useCurrency'
+import Modal from '../Modal'
+
 
 export const Route = createLazyFileRoute('/past')({
   component: PastOrderRoute,
@@ -9,11 +13,20 @@ export const Route = createLazyFileRoute('/past')({
 
 function PastOrderRoute() {
   const [page, setPage] = useState(1)
+  const [focusedOrder, setFocusedOrder] = useState(null)
   const { isLoading, data} = useQuery({
     queryKey: ['past-orders', page],
     queryFn: () => getPastOrders(page),
     staleTime: 30000,
   });
+
+  const { isLoading: isLoadingPastOrder, data: pastOrderData } = useQuery({
+    queryKey: ['past-order', focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder),
+    staleTime: 24 * 60 * 60 * 1000, // one day in milliseconds,
+    enabled: !!focusedOrder, // Only run this query if focusedOrder is set
+  });
+
   if (isLoading) {
     return <h2 className='past-orders'>Loading...</h2>
   }
@@ -26,6 +39,7 @@ function PastOrderRoute() {
             <td>ID</td>
             <td>Date</td>
             <td>Time</td>
+            <td>Action</td>
           </tr>
         </thead>
         <tbody>
@@ -34,6 +48,13 @@ function PastOrderRoute() {
               <td>{order.order_id}</td>
               <td>{order.date}</td>
               <td>{order.time}</td>
+              <td>
+                <button
+                  onClick={() => setFocusedOrder(order.order_id)}
+                >
+                  View Order
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -47,6 +68,45 @@ function PastOrderRoute() {
           Next
         </button>
       </div>
+
+{
+  focusedOrder ? (
+    <Modal>
+      <h2>Order #{focusedOrder}</h2>
+      {!isLoadingPastOrder ? (
+        <table>
+          <thead>
+            <tr>
+              <td>Image</td>
+              <td>Name</td>
+              <td>Size</td>
+              <td>Quantity</td>
+              <td>Price</td>
+              <td>Total</td>
+            </tr>
+          </thead>
+          <tbody>
+            {pastOrderData.orderItems.map((pizza) => (
+              <tr key={`${pizza.pizzaTypeId}_${pizza.size}`}>
+                <td>
+                  <img src={pizza.image} alt={pizza.name} />
+                </td>
+                <td>{pizza.name}</td>
+                <td>{pizza.size}</td>
+                <td>{pizza.quantity}</td>
+                <td>{priceConverter(pizza.price)}</td>
+                <td>{priceConverter(pizza.total)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h2>Loading …</h2>
+      )}
+      <button onClick={() => setFocusedOrder()}>Close</button>
+    </Modal>
+  ) : null
+}
     </div>
   );
  }
---
+++ src/useCurrency.jsx
@@ -0,0 +1,9 @@
+const intl = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+});
+console.log(intl);
+export const priceConverter = (price) => intl.format(price);
+export default function useCurrency(price) {
+    return intl.format(price);
+}
