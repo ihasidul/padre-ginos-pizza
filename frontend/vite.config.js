0802e5bc17e11557875a8134ab6f8bddcf383754
ihasidul <ihasidul@gmail.com>
2025-07-04 22:58:38 +0100

    Remove base from vit config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  base: '/',
   server: {
     proxy: {
       "/api": { target: "http://backend:3000", changeOrigin: true },
