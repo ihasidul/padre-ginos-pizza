5b3fabc102bf7e2ff29b089e637bdd302fe733b0
ihasidul <ihasidul@gmail.com>
2025-07-04 20:40:16 +0100

    Open port 80

--- deployments/terrafrom/vm.tf
+++ deployments/terrafrom/vm.tf
@@ -49,6 +49,20 @@ resource "azurerm_network_security_rule" "ssh_allow" {
   network_security_group_name = azurerm_network_security_group.padre.name
 }
 
+resource "azurerm_network_security_rule" "http_allow" {
+  name                        = "HTTP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.padre.name
+  network_security_group_name = azurerm_network_security_group.padre.name
+}
+
 resource "azurerm_network_interface" "padre" {
   name                = "padre-nic"
   location            = azurerm_resource_group.padre.location
