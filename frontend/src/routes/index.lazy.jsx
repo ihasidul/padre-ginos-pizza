d9141ac5f73fe337f73bd7d736dcb2845f98e6a9
ihasidul <ihasidul@gmail.com>
2025-07-05 02:22:40 +0100

    Update tag line

--- frontend/src/routes/index.lazy.jsx
+++ frontend/src/routes/index.lazy.jsx
@@ -10,7 +10,7 @@ function Index() {
       <div className="index-brand">
         <h1>Padre Gino's</h1>
         {/* <p>Pizza & Art at a location near you</p> */}
-        <p>Where Every Slice is a Masterpiece</p>
+        <p>Bringing Artistry to Every Slice</p>
       </div>
       <ul>
         <li>
